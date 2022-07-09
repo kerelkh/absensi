37fb06888cd3dd8d19526cdcf0c8b1860b06a7ff
kerelkh <kerelkh@gmail.com>
2022-07-09 09:47:15 +0700

    Styling Navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 
-<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+<nav class="text-white from-cyan-500 to-blue-500 bg-gradient-to-bl px-2 sm:px-4 py-2.5 dark:bg-gray-800">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
         <a href="https://flowbite.com/" class="flex items-center">
             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Absensi</span>
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,6 +1,6 @@
 
-<aside class="w-64" aria-label="Sidebar">
-    <div class="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800">
+<aside class="w-64 text-white " aria-label="Sidebar">
+    <div class="overflow-y-auto py-4 px-3 rounded dark:bg-gray-800">
        <ul class="space-y-2">
           @if(Auth::user()->role->id == 2)
           <li>
