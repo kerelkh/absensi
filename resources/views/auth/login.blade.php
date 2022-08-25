4cdb201fc8e3e274b22db5d80f002438b3cdbf8a
kerelkh <kerelkh@gmail.com>
2022-08-25 18:19:44 +0700

    Login Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <title>Login - E-OFFICE</title>
+    <link rel="shortcut icon" href="{{ asset('logos/Logo.png') }}" type="image/png">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 
@@ -33,27 +34,46 @@
         </script>
     @endif
 
-    <div class="w-full h-screen overflow-hidden relative flex justify-center items-center bg-cover bg-center bg-no-repeat" style="background-image: url({{ asset('images/bg1.jpg') }})">
+    <div class="w-full h-screen overflow-hidden flex bg-cover bg-center bg-no-repeat" style="background-image: url({{ asset('images/background-login.svg') }})">
 
-        <form method="POST" class="bg-white p-5 shadow rounded-lg w-[320px] max-w-sm">
-            @csrf
-            <h1 class="mb-5 font-medium text-2xl">Sign In</h1>
-            <div class="relative z-0 w-full mb-6 group">
-                <input type="email" name="email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                <label for="email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
-                @error('email')
-                    <span class="text-red-600 text-xs">* {{ $message }}</span>
-                @enderror
+        <div class="flex-1 h-full grid place-items-center py-10 px-5 relative">
+            <div>
+                <div id="logo" class="w-fit">
+                    <img src="{{ asset('logos/FullLogo_1.png') }}" alt="Logo" class="h-10">
+                </div>
+                <div id="title-login" class="my-10">
+                    <h2 class="font-semibold text-2xl">Login</h2>
+                    <p>To access data, please sign-in using registered account.</p>
+                </div>
+                <div id="login-form" class="my-10">
+                    <form method="POST" class=" w-full max-w-sm">
+                        @csrf
+                        <div class="relative z-0 w-full mb-10 group">
+                            <input type="email" name="email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required autocomplete="off" />
+                            <label for="email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
+                            @error('email')
+                                <span class="text-red-600 text-xs">* {{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="relative z-0 w-full mb-10 group">
+                            <input type="password" name="password" id="password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                            <label for="password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+                            @error('password')
+                                <span class="text-red-600 text-xs">* {{ $message }}</span>
+                            @enderror
+                        </div>
+                        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                      </form>
+                </div>
+                <div id="footer" class="mb-5">
+                    <span class="text-sm font-semibold">Forgot email/password? Contact Admin.</span>
+                </div>
+                <div id="copyright" class="my-10 absolute bottom-0">
+                    <span class="text-sm text-gray-500 italic">&copy; 2022 Copyright E-Office Kepahing</span></span>
+                </div>
             </div>
-            <div class="relative z-0 w-full mb-6 group">
-                <input type="password" name="password" id="password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                <label for="password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
-                @error('password')
-                    <span class="text-red-600 text-xs">* {{ $message }}</span>
-                @enderror
-            </div>
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-          </form>
+        </div>
+        <div class="hidden md:flex-1 h-full md:flex bg-cover bg-center bg-no-repeat" style="background-image: url({{ asset('images/background-image.svg') }})"></div>
 
     </div>
 
